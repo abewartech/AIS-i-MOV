099b16b94e4c3bf799323ce9dc37f2a51675d6c9
Rory <mrrgvm@gmail.com>
2022-11-03 15:56:49 +0100

    added build dor

---
+++ build/lib/ais_parse.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  5 14:24:20 2017
+
+@author: rory
+"""
+import os
+import logging
+import datetime
+import traceback
+import re
+# from re import X
+
+log = logging.getLogger('ais_parse')
+
+class AIS_Parser():
+    # This takes a chunk of raw AIS+metadata messages and then fires off a dictionary of encoded 
+    # AIS message + parsed metadata to rabbit. 
+    # Parsing style depends on the source of data and is controlled by the .env vars
+    def __init__(self):
+        self.routing_key = os.getenv('PRODUCE_KEY')
+        self.ais_meta_style = os.getenv('AIS_STYLE')
+        log.debug('Sending to R-Key: {0}'.format(self.routing_key))
+        log.debug('AIS Style: {0}'.format(self.ais_meta_style))
+        self.multi_msg_dict = {} 
+        self.last_chunk = ''
+
+    def parse_and_seperate(self, msg_chunk, data_logger):
+        # Take a chunk of messages and split them up line by line
+        # Log incoming messages
+        # Parse out the header and footer info
+        # group multiline messages
+        # return a list of dicts 
+
+        # Example Data
+        # !ABVDM,1,1,,B,13=fod0vQv1B5LgdH:vMAJdB00Sa,0*42
+        # !BSVDM,2,1,8,B,5E@;DN02AO;3<HMOJ20Lht84j1A9E=B222222216O@a@M00HtGQSl`3lQ1DT,0*75
+        # !BSVDM,2,2,8,B,p8888888880,2*7E
+        # !ABVDM,1,1,,B,HF<nO80d4v0HtpN0pvs40000000,2*62
+        # !ABVDM,1,1,,B,B8u:Qa0000DwdMs8?LrDio053P06,0*59
+
+        #Place start of message at start of chunk
+        # log.debug(msg_chunk)
+        msg_chunk = msg_chunk.decode('utf-8')
+
+        # This probably needs a little more thinking. How to index chunks when there are different metadata styles? 
+        # How can I guarentee that the line is started in the right place?
+        if self.ais_meta_style == 'IMIS':
+            if msg_chunk[0:3] == '\\g:' or  msg_chunk[0:3] == '\\s:':
+                chunk_list = msg_chunk.split('\r\n')
+
+                #Check if last message is complete. If not then drop it so that it gets handled by 
+                #next incomplete starter.
+                if bool(re.match(r'\!..VD(.*?)[^_]\*[^_][^_]',chunk_list[-1])) == False:
+                    log.debug('Incomplete AIS message in chunk, dropping last message: {0}'.format(chunk_list[-1]))
+                    chunk_list = chunk_list[0:-1]
+            else: 
+                prev_s = self.last_chunk.rfind('\\s')
+                prev_g = self.last_chunk.rfind('\\g')
+                prev_msg = self.last_chunk[max(prev_s, prev_g):]
+                chunk_list = (prev_msg + msg_chunk).split('\r\n')
+
+                #Check if last message is complete. If not then drop it so that it gets handled by 
+                #next incomplete starter.
+                if bool(re.match(r'\!..VD(.*?)[^_]\*[^_][^_]',chunk_list[-1])) == False:
+                    log.debug('Incomplete AIS message in chunk, dropping last message: {0}'.format(chunk_list[-1]))
+                    chunk_list = chunk_list[0:-1]
+                log.debug('Combining prev chunk with this chunk: {0}'.format(chunk_list[0]))
+            # msg_chunk = msg_chunk[msg_chunk.index('\\s'):]
+            
+        # else self.ais_meta_style == 'None':
+        #     msg_chunk = msg_chunk[msg_chunk.index('!'):]
+        #     chunk_list = msg_chunk.split('\n') 
+        else: 
+            #Split the chunk into a list of messages
+            msg_chunk = msg_chunk[msg_chunk.index('!'):]
+            chunk_list = msg_chunk.split('\n') 
+        msg_dict_list = [] 
+        
+        for msg in chunk_list:    
+            try:        
+                if len(msg) < 2:
+                    continue 
+                else:
+                    data_logger.debug(msg)
+                    msg_dict = {}
+                    msg_dict = self.style_parse(msg) 
+                    msg_dict, complete_msg = self.aivdm_parse(msg_dict) 
+                    if complete_msg:
+                        msg_dict_list.append(msg_dict)   
+            except:
+                log.debug('-------------------------------------------------------')
+                log.debug('Problem while parsing AIS message: {0}'.format(str(msg)))
+                log.debug('Parsing Error:' + traceback.format_exc()) 
+                log.debug('Dict: {0}'.format(msg_dict))
+                log.debug('Multi-Dict: {0}'.format(self.multi_msg_dict))
+                log.debug("\n".join(chunk_list))
+
+        self.last_chunk = msg_chunk
+        return msg_dict_list
+     
+# ais_ingestor_1  | 2022-08-31 07:17:15,528 - DEBUG - ais_parse - -------------------------------------------------------
+# ais_ingestor_1  | 2022-08-31 07:17:15,529 - DEBUG - ais_parse - Problem while parsing AIS message: !AIVDM,1,1,,A,D02<HSiitB?b<`E6D0,4*73
+# ais_ingestor_1  | 2022-08-31 07:17:15,529 - DEBUG - ais_parse - Parsing Error:Traceback (most recent call last):
+# ais_ingestor_1  |   File "/usr/local/ais_i_mov/lib/ais_parse.py", line 88, in parse_and_seperate
+# ais_ingestor_1  |     msg_dict, complete_msg = self.aivdm_parse(msg_dict)
+# ais_ingestor_1  |   File "/usr/local/ais_i_mov/lib/ais_parse.py", line 103, in aivdm_parse
+# ais_ingestor_1  |     msg = msg_dict['ais']
+# ais_ingestor_1  | KeyError: 'ais'
+# ais_ingestor_1  | 
+# ais_ingestor_1  | 2022-08-31 07:17:15,529 - DEBUG - ais_parse - Dict: {'server_time': '2022-08-31T07:17:15.528784', 'event_time': '', 'routing_key': 'encoded_ais.aishub.all'}
+
+
+    def aivdm_parse(self, msg_dict):
+        msg = msg_dict['ais']
+        complete_msg = False
+        if msg.split(',')[1] == '1':                
+                msg_dict['multiline'] = False 
+                complete_msg = True
+
+        #Check if first part of multiline message
+        elif msg.split(',')[2] == '1':
+            msg_dict['multiline'] = True
+            self.multi_msg_dict = msg_dict
+            self.multi_msg_dict['msg_id'] = msg.split(',')[3]
+            complete_msg = False
+        
+        #Check if second part of multi msg
+        elif msg.split(',')[2] == '2':
+            #Check if second part belongs with first part
+            if msg.split(',')[3] == self.multi_msg_dict['msg_id']:
+                combo_dict = self.multi_msg_dict
+                combo_dict['ais'] = (self.multi_msg_dict['ais'],msg)
+                combo_dict['header'] = (self.multi_msg_dict['header'],msg_dict['header'])
+                combo_dict['multiline'] = True
+                msg_dict = combo_dict
+                self.multi_msg_dict = {} 
+                complete_msg = True
+            else:
+                log.warning('Dangling multi line message: ' + str(msg))
+        else:
+            log.warning('Unprocessed msg: ' + str(msg))
+        
+        return msg_dict, complete_msg
+    
+    def style_parse(self, msg):
+        # IMIS metadata format. 
+        # \s:CSIR_000,q:u,c:1620731505,i:|X=0|D=1|T=44327.4781489815|P=10.0.100.6:12113|R=IN|E=10000000000000000000|*48\!AIVDM,1,1,,B,33ku82U000OGsfHH:Uv`9j3J0>@<,0*68                         
+        # \g:1-2-1159,s:CSIR,c:1620731662,i:|X=1|D=1|T=44327.4683069097|P=10.0.100.6:12113|R=IN|*46\!AIVDM,2,1,9,A,53Fted42?II@D5=:2204h8Ub2222222222222216:`?1>5D80B0hDh@S0CPh,0*3F            
+        # \g:2-2-1159*51\!AIVDM,2,2,9,A,H8888888880,2*5D  
+        # s: Source
+        # q: 
+        # c: unix timestamp
+        # i: IMIS metadata
+            # X: Data Source?
+            # D: Delay flag?
+            # T: IMIS timestamp?
+            # P: IP Address of source
+            # R: Direction of message? 
+
+        
+        # Bog Standard metadata format
+        # !ABVDM,1,1,,B,13=fod0vQv1B5LgdH:vMAJdB00Sa,0*42
+        # !BSVDM,2,1,8,B,5E@;DN02AO;3<HMOJ20Lht84j1A9E=B222222216O@a@M00HtGQSl`3lQ1DT,0*75
+        # !BSVDM,2,2,8,B,p8888888880,2*7E
+        # !ABVDM,1,1,,B,HF<nO80d4v0HtpN0pvs40000000,2*62
+        # !ABVDM,1,1,,B,B8u:Qa0000DwdMs8?LrDio053P06,0*59
+        
+        log.debug('Parsing: {0}'.format(msg))
+        parsed_line = {}
+        if self.ais_meta_style == 'IMIS':
+            meta = msg[: msg.index('\!')+1]
+            ais = msg[msg.index('\!')+1 :]
+            meta_list = meta.strip('\\').split(',')
+            meta_dict = {}
+            for item in meta_list:
+                meta_dict[item[: item.index(':')]] = item[item.index(':') +1:]
+
+            parsed_line['server_time'] = datetime.datetime.utcnow().isoformat()
+            parsed_line['header'] = meta_dict
+            parsed_line['routing_key'] = self.routing_key
+            try:
+                parsed_line['event_time'] =  datetime.datetime.fromtimestamp(int(meta_dict['c'])).isoformat()
+            except:
+                log.debug('No timestamp on this message')
+            parsed_line['ais'] = ais
+
+        else:
+            parsed_line['ais'] = msg
+            parsed_line['header'] = None 
+            parsed_line['server_time'] = datetime.datetime.utcnow().isoformat()
+            parsed_line['event_time'] = datetime.datetime.utcnow().isoformat()
+            parsed_line['routing_key'] = self.routing_key
+         
+        return parsed_line   
+
---
+++ build/main.py
@@ -0,0 +1,172 @@
+    #!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 24 15:05:19 2017
+
+@author: rory
+"""
+import sys
+import time
+import argparse
+import logging 
+import os
+import traceback
+import socket
+import datetime
+import re
+
+from kombu import Connection, Exchange, Queue, binding
+
+import lib.funcs
+import lib.rabbit
+import lib.ais_parse
+
+# This whole thing is supposed to take the raw AIS, do a high level split on it: 
+    # - Meta-data 
+    # - Raw AIS
+    # - Header/Footer
+    # - Routing key
+# This is then written to a log file and passed to rabbit.
+
+
+log = logging.getLogger('main')
+# log.setLevel('DEBUG')
+def setup_logging():
+    '''
+    Setup some nice logging to store incoming data
+    '''
+    logger = logging.getLogger(os.getenv('LOG_NAME'))
+    if not len(logger.handlers):
+        logger.setLevel(logging.INFO)
+        log_path = os.getenv('LOG_DIR')
+        log_file = os.getenv('LOG_NAME')
+        if not os.path.exists(log_path): 
+            os.mkdir(log_path)
+        log_file_handler = logging.handlers.TimedRotatingFileHandler(os.path.join(log_path,log_file), when='midnight')
+        log_file_handler.setFormatter( logging.Formatter('%(asctime)s: %(message)s') )
+        log_file_handler.setLevel(logging.DEBUG)
+        logger.addHandler(log_file_handler)
+    return logger
+
+def read_socket(data_logger):
+    # Create a TCP/IP socket
+    log.info('Opening socket') 
+    sock = socket.socket()
+    log.info('Setting socket timeout to %s seconds.',os.getenv('SOCKET_TIMEOUT'))
+    sock.settimeout(int(os.getenv('SOCKET_TIMEOUT')))
+    server_address = (os.getenv('SOURCE_HOST'),int(os.getenv('SOURCE_PORT')))
+    log.info('Connecting to '+str(server_address)) 
+    sock.connect(server_address)
+    
+    # Create RabbitMQ publisher
+    rabbit_publisher = lib.rabbit.Rabbit_Producer()
+    ais_parser = lib.ais_parse.AIS_Parser()
+    try:
+        log.info('Streaming AIS...') 
+
+        while True:
+            # https://stackoverflow.com/questions/47758023/python3-socket-random-partial-result-on-socket-receive
+            data_chunk = b''
+            while True:
+                try:
+                    chunk = sock.recv(int(os.getenv('CHUNK_BYTES')))
+                    log.debug('Chunk received')
+                    valid_chunk = re.search(b'[0]\*[0-9a-fA-F]+$', chunk)
+                    # if not chunk:
+                    #     log.debug('Complete chunk, reading more: len = {}'.format(len(chunk)))
+                    #     break
+                    if valid_chunk is not None:
+                        log.debug('Complete chunk, reading more: len = {}'.format(len(chunk)))
+                        break
+                    data_chunk += chunk
+                except socket.error:
+                    # sock.close()
+                    break
+                
+
+            log.debug('----------------')
+            chunk_len = len(data_chunk)
+            if chunk_len <  316:
+                row_len = chunk_len
+            else:
+                row_len = 316
+            log.debug('Chunk size: {0}'.format(chunk_len))
+            log.debug('Chunk start: {0} ...'.format(data_chunk[0:row_len+1]))
+            log.debug('Chunk end: ... {0}'.format(data_chunk[-row_len-1:]))
+            if len(data_chunk) > 2:
+                try:
+                    msg_list = ais_parser.parse_and_seperate(data_chunk,data_logger)
+                except:
+                    log.info('Problem parsing message')
+                for msg in msg_list:
+                    rabbit_publisher.produce(msg)
+                    # log.info(msg['ais'])
+                    pass
+            else:
+                continue
+    except:
+        log.error('Error in AIS streaming:' + traceback.format_exc())
+        log.error('Last MSG: ' + str(msg))
+    finally:
+        log.warning('Closing socket')
+        sock.close() 
+        time.sleep(10)
+ 
+def do_work():  
+    '''
+    This opens a socket to the server that is feeding AIS.
+    Collect multiline messages into a single dict.
+    Adds a routing key, server time etc to the message
+    Publishes message onto RabbitMQ
+    '''
+    log.info('Getting ready to do work...') 
+    data_logger = setup_logging()
+    try:
+        while True:
+            read_socket(data_logger)
+    except:
+        log.error('Error in main loop:') 
+        log.error(traceback.format_exc())
+        time.sleep(10)
+
+
+    log.info('Worker shutdown...')
+
+def main(args):
+    '''
+    Setup logging, and args, then "do_work"
+    '''
+    logging.basicConfig(
+        stream=sys.stdout,
+        format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
+        level=getattr(logging, args.loglevel))
+
+    log.setLevel(getattr(logging, args.loglevel))
+    log.info('ARGS: {0}'.format(ARGS)) 
+    do_work()
+    log.warning('Script Ended...') 
+
+if __name__ == "__main__":
+    '''
+    This takes the command line args and passes them to the 'main' function
+    '''
+    PARSER = argparse.ArgumentParser(
+        description='Run the DB inserter')
+    PARSER.add_argument(
+        '-f', '--folder', help='This is the folder to read.',
+        default = None, required=False)
+    PARSER.add_argument(
+        '-ll', '--loglevel', default='INFO',
+        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+        help="Set log level for service (%s)" % 'INFO')
+    ARGS = PARSER.parse_args()
+    try:
+        main(ARGS)
+    except KeyboardInterrupt:
+        log.warning('Keyboard Interrupt. Exiting...')
+        os._exit(0)
+    except Exception as error:
+        log.error('Other exception. Exiting with code 1...')
+        log.error(traceback.format_exc())
+        log.error(error)
+        os._exit(1)
